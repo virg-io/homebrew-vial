7d48fc357712da7c34e756eba6a29d727043b580
Filip Sobalski <pinkeen@gmail.com>
2019-07-19 22:34:52 +0200

    ...

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ ! -f "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg" ] || sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+[[ ! -f "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg" ]] || sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 ./buttler info
 ./buttler build-tap
 ./buttler upload
