ec72682479767bdfb0bbd57115c1f0d8ca8886c4
Filip Sobalski <pinkeen@gmail.com>
2019-07-19 23:39:43 +0200

    ...

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,6 +5,13 @@
 git config --global user.name "Buttler@TravisCI"
 git config --global user.email buttler@travis-ci.org
 
+GIT_ORIGIN_URI_OLD="$(git remote get-url origin)"
+GIT_ORIGIN_URI_NEW="$(echo "$GIT_ORIGIN_URI_OLD" | sed -E 's~https://github.com/|(ssh\://)?git@github.com\:~https://'"$GITHUB_USERNAME:$GITHUB_TOKEN"'@github.com/~g')"
+
+echo "Rewriting git origin to use token: $GIT_ORIGIN_URI_OLD -> $GIT_ORIGIN_URI_NEW"
+
+git remote set-url origin "$GIT_ORIGIN_URI_NEW"
+
 export GIT_BRANCH="$TRAVIS_BRANCH"
 
 ./buttler info
